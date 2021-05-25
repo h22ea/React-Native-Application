ff7fa835f610168339b61786ef02f2b7c0b68bbd
Horik <h22ea@icloud.com>
2021-05-25 11:47:23 +0300

    Enclosed Token in .env file

--- src/api/giphy.js
+++ src/api/giphy.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const GIPHY_TOKEN = process.env.GIPHY_TOKEN;
+
 export default axios.create({
     baseURL: 'https://api.giphy.com/v1/stickers',
     headers: {
-        Authorization: 'Bearer Y8N2iAFFHHkUmEQvsnutVF3t1vSEll2Q'
+        Authorization: GIPHY_TOKEN
     },
 })
